fe815a84a5d16b2f1f447aea02e9c7663406bbca
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-10-10 13:03:54 -0400

    Started
    Started repo for personal site.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$("document").ready(function(){
+	$(".container_12").hide().fadeIn(5000);
+	$("#box1").hide().slideDown(7000);
+	$("#box2").hide().slideDown(9000);
+	$("#box3").hide().slideDown(11000);
+	$("p, h1, h2").hide().fadeIn(19000);
+});
+
